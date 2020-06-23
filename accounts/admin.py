b8ca8bc683c49d24d653927727d2708fea93e68c
wookpyo <wookpyo@gmail.com>
2020-06-23 20:23:27 +0900

    pt5

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Customer
+from .models import *
 
 admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
