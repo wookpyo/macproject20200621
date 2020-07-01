3394b32cde74dfd2edcd9741db8d88c2d6a859ee
wookpyo <wookpyo@gmail.com>
2020-07-01 17:45:45 +0900

    pt16

--- accounts/views.py
+++ accounts/views.py
@@ -29,6 +29,9 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			Customer.objects.create(
+				user=user,
+				)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -77,8 +80,18 @@ def home(request):
 
 	return render(request, 'accounts/dashborad.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-	context = {}
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
 	return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
